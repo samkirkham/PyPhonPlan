3f817cc4c2aedc54f88c1f6d742b338d83e103c8
Sam Kirkham <s.d.kirkham@gmail.com>
2026-02-28 00:00:18 +0000

    Use above threshold values, expects dt=0.001 but gets DFT field dt, need to fix

--- examples/field_to_task.py
+++ examples/field_to_task.py
@@ -7,6 +7,7 @@ TaskDynamics pipeline end-to-end.
 """
 
 from pyphonplan import DynamicField, Targets, TaskDynamics
+from pyphonplan.viz import plot_field_heatmap, plot_field_surface
 
 # Dynamic field simulation
 field = DynamicField(x_min=-10, x_max=10, step_size=0.1)
@@ -15,12 +16,13 @@ field.set_kernel(c_exc=1, c_inh=1, c_global=0.05, sigma_exc=1.0, sigma_inh=5.0, 
 field.add_input("inp", amplitude=5, position=5, width=1.0, start=50, end=150)
 field.solve(t_start=0, t_end=250, dt=1, tau=25.0, h=-2.0)
 
-# Extract target trace
-targets = Targets(field, positions=[5.0])
+# plot to check activation
+plot_field_heatmap(field.time, field.x, field.activation)
+plot_field_surface(field.time, field.x, field.activation, threshold=0.0)
 
-# above_threshold=False gives a regularly-spaced time array needed by the ODE
-# NOTE: this is not ideal -> need to use above_threshold values but also handle edge cases in which there's a gap in above threshold activation at some point -> about to refactor this.
-param_peak, act_peak, time = targets.peak_activation(above_threshold=False)
+# Extract target trace (above_threshold filters to active period only)
+targets = Targets(field)
+param_peak, act_peak, time = targets.peak_activation(above_threshold=True)
 
 td = TaskDynamics()
 td.solve_from_trace(time, param_peak, k=2000.0)
